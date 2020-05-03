672a9d52f58cb86d04d6ab09640b9e019a284609
julweng <julweng@gmail.com>
2020-05-03 12:53:59 -0700

    Updates

---
+++ precache-manifest.5d74def225d6452425631744220a66a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abca5d4cbdf810db14a92598664693f1",
+    "url": "/covid_tracker/index.html"
+  },
+  {
+    "revision": "9aea49aaf9868f119c7b",
+    "url": "/covid_tracker/static/css/main.445283f9.chunk.css"
+  },
+  {
+    "revision": "17fcaa53de940afc39e3",
+    "url": "/covid_tracker/static/js/2.f0331056.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid_tracker/static/js/2.f0331056.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aea49aaf9868f119c7b",
+    "url": "/covid_tracker/static/js/main.2c760a9a.chunk.js"
+  },
+  {
+    "revision": "1b0678fa0ffc5afc9495",
+    "url": "/covid_tracker/static/js/runtime-main.f71848b3.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid_tracker/static/media/image.d7265326.png"
+  }
+]);
