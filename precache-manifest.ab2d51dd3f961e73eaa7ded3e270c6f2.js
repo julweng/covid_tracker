5dbe70b3164284ca4d7062588591eec76c9a3fdf
julweng <julweng@gmail.com>
2022-06-21 17:51:41 -0700

    Updates

---
+++ precache-manifest.ab2d51dd3f961e73eaa7ded3e270c6f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c64a6e3dc7140ea655c4c8e56ea1050",
+    "url": "/covid_tracker/index.html"
+  },
+  {
+    "revision": "938d62f27f50825d99a3",
+    "url": "/covid_tracker/static/css/main.445283f9.chunk.css"
+  },
+  {
+    "revision": "0da0e9fc52ea7a822e41",
+    "url": "/covid_tracker/static/js/2.6cf6d6d5.chunk.js"
+  },
+  {
+    "revision": "4ed2d4f00cf8324f872f01907affe619",
+    "url": "/covid_tracker/static/js/2.6cf6d6d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "938d62f27f50825d99a3",
+    "url": "/covid_tracker/static/js/main.27c070bd.chunk.js"
+  },
+  {
+    "revision": "1b0678fa0ffc5afc9495",
+    "url": "/covid_tracker/static/js/runtime-main.f71848b3.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid_tracker/static/media/image.d7265326.png"
+  }
+]);
